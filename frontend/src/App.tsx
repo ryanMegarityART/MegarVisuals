91d474ea2fcd16353747f8b7b697d4b9911000d1
ryanMegarityART <ryan.megarity@pshealthgroup.com>
2022-10-15 12:45:33 +0100

    move logic around

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import logoBlue from "./assets/logoBlue.jpg";
 import { TopNav } from "./Components/Nav/TopNav";
-import { Canvas, canvasDefaultProps } from "./Components/Visualisers/Canvas";
+import {
+  Canvas,
+  canvasDefaultSizeProps,
+} from "./Components/Visualisers/Canvas";
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [context, setContext] = useState<any>(null);
+
+  useEffect(() => {
+    if (context) {
+      context.beginPath();
+      context.arc(50, 50, 50, 0, 2 * Math.PI);
+      context.fill();
+    }
+  }, context);
+
   return (
     <div className="App">
       <TopNav />
-      <Canvas {...canvasDefaultProps} />
+      <Canvas
+        canvasRef={canvasRef}
+        context={context}
+        setContext={setContext}
+        {...canvasDefaultSizeProps}
+      />
     </div>
   );
 }
--- frontend/src/Components/Visualisers/Canvas.tsx
+++ frontend/src/Components/Visualisers/Canvas.tsx
@@ -20,16 +20,10 @@ export const Canvas = ({
   context,
   setContext,
 }: CanvasProps) => {
-
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
-      const context = canvas.getContext("2d");
-      if (context) {
-        context.beginPath();
-        context.arc(50, 50, 50, 0, 2 * Math.PI);
-        context.fill();
-      }
+      setContext(canvas.getContext("2d"));
     }
   });
 
