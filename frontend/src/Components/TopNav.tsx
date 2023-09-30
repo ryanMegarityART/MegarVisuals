42d3db9a8b2b53ebe44f7ceeef2de4cb38620d52
Ryan Megarity <ryan.megarity@immerse.io>
2023-09-30 17:08:48 +0100

    Fix mobile nav styling

--- frontend/src/Components/TopNav.tsx
+++ frontend/src/Components/TopNav.tsx
@@ -5,7 +5,7 @@ import Image from "react-bootstrap/Image";
 
 export const TopNav = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">
           <Image
@@ -16,22 +16,12 @@ export const TopNav = () => {
           ></Image>
           <span className="p-3">MegarVisuals</span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto"></Nav>
-          <Nav>
-            <Nav.Link
-              href="https://github.com/ryanMegarityART/MegarVisuals"
-              target="_blank"
-            >
-              <img
-                src="/images/gitHub.png"
-                alt="github-image"
-                className="p-img"
-              />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav.Link
+          href="https://github.com/ryanMegarityART/MegarVisuals"
+          target="_blank"
+        >
+          <img src="/images/gitHub.png" alt="github-image" className="p-img" />
+        </Nav.Link>
       </Container>
     </Navbar>
   );
